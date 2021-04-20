da445774e0a0987a99b0b99bb8f95b07975116c6
brunoamaia <brunoamaia94@gmail.com>
2021-04-20 14:40:36 -0300

    create static page using local data

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,14 +1,62 @@
 import Head from 'next/head'
 
-export default function Home() {
+import localData from '../../server'
+
+interface serverData {
+  episodes: [
+    {
+      id: string,
+      title: string,
+      members: string,
+      published_at: string,
+      thumbnail: string,
+      description: string,
+      file: {
+        url: string,
+        type: string,
+        duration: number,
+      }
+    }
+  ]
+}
+
+const hash = global.window && window.location.hash
+
+export default function Home(props: serverData) {
   return (
     <div>
       <Head>
         <title>Podcastr</title>
       </Head>
 
+      <main>
+        <strong>Episódios</strong>
+        {props.episodes.map((episode) => (
+          <p key={episode.file.duration} >{episode.title}</p>
+        ))}
+        <br/> <br/>
+      </main>
+
       <footer>
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  /* // localhost [development] - access local server (json server)
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json() */ 
+
+ // vercel (local data)
+  const data = localData
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
+
+  // revalidate: tempo que a página irá se atualizar (em segundos)
 }
